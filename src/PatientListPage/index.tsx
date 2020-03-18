f0d29161d9650a8a3737244a32d7fb8501aa9149
Tuukka Peuraniemi <tuukka.peuraniemi@terveystalo.com>
2020-03-18 13:03:57 +0200

    Patients in state can return undefined if key is invalid

--- src/PatientListPage/index.tsx
+++ src/PatientListPage/index.tsx
@@ -51,16 +51,19 @@ const PatientListPage: React.FC = () => {
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {Object.values(patients).map((patient: Patient) => (
-            <Table.Row key={patient.id}>
-              <Table.Cell>{patient.name}</Table.Cell>
-              <Table.Cell>{patient.gender}</Table.Cell>
-              <Table.Cell>{patient.occupation}</Table.Cell>
-              <Table.Cell>
-                <HealthRatingBar showText={false} rating={1} />
-              </Table.Cell>
-            </Table.Row>
-          ))}
+          {Object.values(patients).map(
+            patient =>
+              patient && (
+                <Table.Row key={patient.id}>
+                  <Table.Cell>{patient.name}</Table.Cell>
+                  <Table.Cell>{patient.gender}</Table.Cell>
+                  <Table.Cell>{patient.occupation}</Table.Cell>
+                  <Table.Cell>
+                    <HealthRatingBar showText={false} rating={1} />
+                  </Table.Cell>
+                </Table.Row>
+              )
+          )}
         </Table.Body>
       </Table>
       <AddPatientModal
